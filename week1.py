6c0a1d354870b3dfdaa352299508595dfcea4d10
DaveGithua <githua999@gmail.com>
2024-11-06 19:17:41 +0300

    Intro to Variables

---
+++ week1.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+# Print out a personalized message
+print(f"Hello, {name}! You are {age} years old and live in {location}.")
